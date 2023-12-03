6c5aa3b63b4634d73c5dd07eecdbc41e4ecd9211
Niels <7136117+TheDutchDev@users.noreply.github.com>
2023-12-03 10:27:42 +0100

    add missing xdocs

--- src/Base/AtlasPed.cs
+++ src/Base/AtlasPed.cs
@@ -86,6 +86,9 @@ public class AtlasPed : AsyncPed, IAtlasServerPed
         Alt.OnPedHeal += OnPedHeal;
     }
 
+    /// <summary>
+    /// Destructor
+    /// </summary>
     ~AtlasPed( )
     {
         CurrentTask?.OnStop( this );
